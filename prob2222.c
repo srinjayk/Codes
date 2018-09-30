4432b774136010970bd8bf45e192056e5109a371
Srinjay Kumar <35092522+srinjayk@users.noreply.github.com>
2018-09-30 11:10:01 +0530

    Update

--- prob2222.c
+++ prob2222.c
@@ -18,13 +18,11 @@ int main()
     	scanf("\n");
     }
 
-    for (i = 0; i < a; ++i)
+    /*for (i = 0; i < a; ++i)
     {
-    	/* code */
     	for (j = 0; j < a; ++j)
     	{
-    		/* code */
-    		//printf("%d ",arr[i][j]);
+    		printf("%d ",arr[i][j]);
     	}
     }
 
@@ -32,19 +30,15 @@ int main()
 
     for (i = 0; i < 729; ++i)
     {
-    	/* code */
     	b[i]=0;
     }
     
     for (i = 0; i < a; ++i)
     {
-    	/* code */
     	for (j = 0; j < a; ++j)
     	{
-    		/* code */
     		if (arr[i][j]!=0)
     		{
-    			/* code */
     			int c=arr[i][j];
     			b[i*81+j*9+c]=1;
     		}
@@ -53,17 +47,55 @@ int main()
     
     for (i = 0; i < 729; ++i)
     {
-    	/* code */
-    	//printf("%d\n",b[i]);
+    	printf("%d\n",b[i]);
     }
     
     for (i = 0; i < 729; ++i)
     {
-    	/* code */
-    	//printf("%d\n",b[i]);
+    	printf("%d\n",b[i]);
     	if(b[i]==1)
     	{
     		printf("%d 0\n",i);
+    		r++;
+    	}
+    }*/
+
+
+    for (i = 0; i < 9; ++i)
+    {
+        /* code */
+        for (j = 0; j < 9; ++j)
+        {
+            /* code */
+            //scanf("%d ",&arr[i][j]);
+            if (arr[i][j]!=0)
+            {
+                /* code */
+                int c=arr[i][j];
+                printf("%d 0\n",(i*81+j*9+c));
+                r++;
+
+            }
+        }
+        scanf("\n");
+    }
+
+    for (k = 0; k < 9; ++k)
+    {
+    	/* code */
+    	for (j = 0; j < 9; ++j)
+    	{
+    		/* code */
+    		for (m = 0; m < 9; ++m)
+    		{
+    			
+    				/* code */
+
+    				printf("%d ",(j*81+m*9+k) );
+
+    		
+    		}
+    		printf("0\n");
     		r++;
     	}
     }
@@ -74,36 +106,15 @@ int main()
     	for (j = 0; j < 9; ++j)
     	{
     		/* code */
-    		for (m = 0; m < 8; ++m)
+    		for (m = 0; m < 9; ++m)
     		{
-    			for (l = m+1; l < 9; ++l)
-    			{
+    			
     				/* code */
+    				printf("%d ",(m*81+j*9+k) );
 
-    				printf("%d %d 0\n",-(j*81+m*9+k),-(j*81+l*9+k) );
-    				r++;
-
-    			}
     		}
-    	}
-    }
-
-    for (k = 0; k < 9; ++k)
-    {
-    	/* code */
-    	for (j = 0; j < 9; ++j)
-    	{
-    		/* code */
-    		for (m = 0; m < 8; ++m)
-    		{
-    			for (l = m+1; l < 9; ++l)
-    			{
-    				/* code */
-    				printf("%d %d 0\n",-(l*81+j*9+k),-(m*81+j*9+k) );
-    				r++;
-
-    			}
-    		}
+    		printf("0 \n");
+    		r++;
     	}
     }
 
@@ -121,27 +132,57 @@ int main()
     				for (l = 0; l < 3; ++l)
     				{
     					/* code */
-    					if ((l==2)&&(m==2))
-    					{
-    						break;
-    					}
-    					for (p = m+1; p < 3; ++p)
-    					{
-    						/* code */
-    						for (q = l+1; q < 3; ++q)
-    						{
+    					
     							/* code */
-    							printf("%d %d 0\n",-((i+l)*81+(j+m)*9+k),-((i+p)*81+(j+q)*9+k) );
-    							r++;
-    						}
-    					}
+    							printf("%d ",((i+l)*81+(j+m)*9+k));
+    						
     				}
     			}
+    			printf("0 \n");
+    			r++;
 
     		}
     		
     	}
     }
+    
+    for (k = 0; k < 9; ++k)
+    {
+    	/* code */
+    	for(m = 0 ; m<9 ;m++)
+    	{
+    		printf("%d ",(m*81+m*9+k));
+    	}
+    	printf("0 \n");
+    	r++;
+    }
+
+    for (k = 0; k < 9; ++k)
+    {
+    	/* code */
+    	for(m = 0 ; m<9 ;m++)
+    	{
+    		printf("%d ",(m*81+(8-m)*9+k));
+    	}
+    	printf("0 \n");
+    	r++;
+    }
+
+    for(i=0;i<9;i++)
+    {
+    	for(j=0;j<9;j++)
+    	{
+    		for(l=0;l<8;l++)
+    		{
+    			for(m=l+1;m<9;m++)
+    			{
+    				printf("%d %d 0\n",-(i*81+j*9+l),-(i*81+j*9+m));
+    				r++;
+    			}
+    		}
+    	}
+    }
+
     printf("%d\n",r );
 
 
